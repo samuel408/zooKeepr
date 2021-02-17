6ec915fd94cdc0fe00aac3fa5830ac4caf80a6ad
Samuel <samuel@Samuels-MacBook-Pro.local>
2021-02-16 20:14:03 -0800

    Add Heroku

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express =  require('express');
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 const { animals } = require('./data/animals');
 
@@ -58,6 +60,6 @@ function filterByQuery(query, animalsArray) {
 
   });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+  app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
