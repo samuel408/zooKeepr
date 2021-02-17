95fd25aca5d38743f57c8f1eb2e2f2b4cea98b71
Samuel <samuel@Samuels-MacBook-Pro.local>
2021-02-16 20:40:46 -0800

    app now uses heroku and distenguishes JSON file by request

--- server.js
+++ server.js
@@ -46,7 +46,10 @@ function filterByQuery(query, animalsArray) {
 
 
 
-
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
 
 
 
@@ -58,6 +61,14 @@ function filterByQuery(query, animalsArray) {
     res.json(results);
     console.log(req.query)
 
+  });
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.send(404);
+    }
   });
 
   app.listen(PORT, () => {
